0b995493b777fb8ce2c7d64ee4752dc22cc4ed42
unknown <kindermolina98@gmail.com>
2025-12-09 13:18:55 -0300

    transition to winapi32 application;

--- src/builder/models.cpp
+++ src/builder/models.cpp
@@ -20,14 +20,7 @@ void limpiar_ANSI() {
     printf("\033[2J"); // \033[2J: Borra toda la pantalla
     printf("\033[H");  // \033[H: Mueve el cursor a la esquina superior izquierda (1,1)
     fflush(stdout);   // Asegura que se envíe la salida inmediatamente
-}
-
-void limpiarConsola() {
-#ifdef _WIN32 // Si es Windows
-    system("cls");
-#else // Si es Linux, macOS u otro (Unix-like)
-    system("clear");
-#endif
+    //std::cout << "\033[31m" << "This text is red." << "\033[0m" << std::endl;
 }
 
 float dimension(int i, int j){
@@ -41,7 +34,7 @@ float dimension(int i, int j){
 }
 
 void least_squares(MatrixXd Data, MatrixXd PHI, VectorXd dens, float prev_alpha, float RMS_rel_prev) {
-    //comparing
+
     VectorXd g_c = PHI * dens;
     add_csv(g_c);
     VectorXd diffs = Data.col(3) - g_c;
@@ -50,37 +43,30 @@ void least_squares(MatrixXd Data, MatrixXd PHI, VectorXd dens, float prev_alpha,
     double RMS_rel = RMS / Data.col(3).norm();
     float SSE = 0.06;
     float alpha = 0.0;
-    //std::cout << "\nRMS:\n" << RMS << std::endl;
-    //std::cout << "\nRMS_rel_prev:\n" << RMS_rel_prev << std::endl;
 
     if (RMS_rel < SSE){
         std::cout << "\nRMS_rel:\n" << RMS_rel << std::endl;
         std::cout << "\nalpha:\n" << alpha << std::endl;
 
-        std::cout << "is okey" << std::endl;
-        std::cout << "\ng_c:\n" << g_c << std::endl;
-        std::cout << "\nDensities:\n" << dens << std::endl;
-        std::cout << "\nStoped Process:\n" << std::endl;
+        //std::cout << "\ng_c:\n" << g_c << std::endl;
+        //std::cout << "\nDensities:\n" << dens << std::endl;
         alpha = prev_alpha * 0.99;
-        double lambda = 1e-3;
-        int n = PHI.cols();
-        MatrixXd I_lamb = lambda * Eigen::MatrixXd::Identity(n,n);
-        std::cout << "\nI_lambda:\n" << I_lamb << std::endl;
     } else {
         if(float(RMS_rel < float(RMS_rel_prev))){
             alpha = prev_alpha * 0.99;
+            double lambda = 1e-3;
+            int n = PHI.cols();
+            MatrixXd I_lamb = lambda * Eigen::MatrixXd::Identity(n,n);
 
-            MatrixXd ATA = PHI.transpose() * PHI;
+            MatrixXd ATA = (PHI.transpose() * PHI);
             VectorXd ATb = PHI.transpose() * diffs;
             VectorXd diffs_sigma = ATA.inverse() * ATb;
             VectorXd new_dens = dens + (alpha) * diffs_sigma;
 
-            std::cout << "\033[2K\r";
-            std::cout << "\033[A";
-            std::cout << "\033[2K\r";
-
+            limpiar_ANSI();
             std::cout << "RMS_rel:" << RMS_rel << "\n";
-            std::cout << "alpha:" << alpha << "\r" << std::flush;
+            std::cout << "alpha:" << alpha << "\n";
+
             least_squares(Data, PHI, new_dens, alpha, RMS_rel);
         }
     }
--- src/index.cpp
+++ src/index.cpp
@@ -3,15 +3,27 @@
 #include <stdio.h>
 #include <math.h>
 #include <Eigen/Dense>
+#include <windows.h>
+#include <winuser.h>
 
 #include "models.h"
 #include "export.h"
 #include "plotting.h"
 
+#ifndef UNICODE
+#define UNICODE
+#endif 
+
 using Eigen::VectorXd;
 using Eigen::MatrixXd;
 
-int main () {
+LRESULT CALLBACK wndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+
+int WINAPI WinMain (HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR pCmdLine, int nCmdShow) {
+    const char CLASS_NAME[]  = "Sample Window Class";
+    HWND hwnd;
+    MSG msg;
+
     std::string elements = "polygons";
     VectorXd L(10);
     MatrixXd Data(10,4);
@@ -24,13 +36,58 @@ int main () {
     
     L << 1e-5, 1.5e-5, 1.7e-5, 1.7e-5, 1.84e-5, 1.33e-5, 1.29e-5, 1.21e-5, 1.2e-5, 1.17e-5;
     Data.col(3) = L;
-    make_csv(Data.col(0));
-    add_csv(Data.col(3));
+    //make_csv(Data.col(0));
+    //add_csv(Data.col(3));
     //implement();
-    initial_model(elements, Data);
+    //initial_model(elements, Data);
+    //plot("here.txt");
+    
+    WNDCLASS wc = {};
+    wc.lpfnWndProc   = wndProc;
+    wc.hInstance     = hInstance;
+    wc.lpszClassName = CLASS_NAME;
 
-    plot("here.txt");
+    if(!RegisterClass(&wc)){
+        MessageBox(NULL, "Window Registration Failed!", "Error!", MB_ICONEXCLAMATION | MB_OK);
+        return 0;
+    }
+    hwnd = CreateWindowEx(
+        0,
+        CLASS_NAME,
+        "Learn to Program Windows",
+        WS_OVERLAPPEDWINDOW,
+        CW_USEDEFAULT, CW_USEDEFAULT, 480, 240,
+        NULL, NULL, hInstance, NULL);
+
+    if (hwnd == NULL) {
+        return 0;
+        }
+
+    ShowWindow(hwnd, nCmdShow);
+    UpdateWindow(hwnd);
+    while (GetMessage(&msg, NULL, 0, 0) > 0) {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+        }
 
     return 0;
 }
 
+LRESULT CALLBACK wndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
+    switch (uMsg) {
+        case WM_DESTROY:
+            PostQuitMessage(0);
+            return 0;
+        case WM_PAINT:
+            {
+                PAINTSTRUCT ps;
+                HDC hdc = BeginPaint(hwnd, &ps);
+
+                FillRect(hdc, &ps.rcPaint, (HBRUSH) (COLOR_WINDOW+1));
+
+                EndPaint(hwnd, &ps);
+            }
+            return 0;
+        }
+        return DefWindowProc(hwnd, uMsg, wParam, lParam);
+}
