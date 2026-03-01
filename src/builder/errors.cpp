51df3eaf4315c7f32e1c10fd37a5a1534659e74d
unknown <kindermolina98@gmail.com>
2026-03-01 20:41:27 -0300

    I will start building customized errors;

--- src/builder/errors.cpp
+++ src/builder/errors.cpp
@@ -1,4 +1,24 @@
+
+#include <iostream>
+#include <stdexcept>
+#include <string>
+
+
 void set_error(int code){
     char buffer[256];
 }
+
+struct MyCustomException : public std::runtime_error {
+    using std::runtime_error::runtime_error; 
+};
+
+/*
+void checkValue(int value) {
+    if (value < 0) {
+        // Throw an instance of the custom exception with a descriptive message
+        throw MyCustomException("Value cannot be negative: " + std::to_string(value));
+    }
+    std::cout << "Value is valid: " << value << std::endl;
+}
+*/
 
